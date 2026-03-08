502bfbc90f53bdcac6cdb254de11055cdd184264
Joel Calm <joelcalm44@gmail.com>
2026-03-08 10:11:48 +0100

    camera utils

--- utils/camera_utils.py
+++ utils/camera_utils.py
@@ -11,6 +11,7 @@
 
 from scene.cameras import Camera
 import numpy as np
+from PIL import Image
 from utils.general_utils import PILtoTorch
 from utils.graphics_utils import fov2focal
 import torch
@@ -47,11 +48,14 @@ def loadCam(args, id, cam_info, resolution_scale):
     if resized_image_rgb.shape[1] == 4:
         loaded_mask = resized_image_rgb[3:4, ...]
 
+    objects = cam_info.objects
+    if objects is not None:
+        objects = objects.resize(resolution, Image.NEAREST)
     return Camera(colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T, 
                   FoVx=cam_info.FovX, FoVy=cam_info.FovY, 
                   image=gt_image, gt_alpha_mask=loaded_mask,
                   image_name=cam_info.image_name, uid=id, data_device=args.data_device,
-                  objects=torch.from_numpy(np.array(cam_info.objects)))
+                  objects=torch.from_numpy(np.array(objects)))
 
 def cameraList_from_camInfos(cam_infos, resolution_scale, args):
     camera_list = []
